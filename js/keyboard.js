9eb19d5b0f91fb50e36c95405dfcd9b98117c119
Maxime <maximemurgia@gmail.com>
2015-06-09 09:01:07 +0200

    keyboard improved
    keyboard service is ready for test

--- js/keyboard.js
+++ js/keyboard.js
@@ -6,35 +6,45 @@ var Keyboard = function() {
 	*/
 	
 	this._keyPressed = [];
-	
-	/*
-	*		We get Downed keys 
-	*		This function is repeated until the key is pressed. So we need to prevent this : pressedButton
-	*/
-	var self = this;
-	$("*").keydown(function(codeKey){
-		
-		self._keyPressed[codeKey.keyCode] = 1;
-		console.log("[DBG] > Key UP : " + codeKey.keyCode);
-		console.log ("[DBG] " + self._keyPressed);
-	});
-	
-	/*
-	*		We get Up key
-	*/
-	$("*").keyup(function(codeKey){
-		console.log("[DBG] > Key DW : " + codeKey.keyCode);
-		self._keyPressed[codeKey.keyCode] = 0;
-		console.log ("[DBG] " + self._keyPressed);
-	});
+	this._repeatableKey = [];
+	this._repeatableKeyTimeOutID = [];
+
 };
 Keyboard.prototype.addKeyListener = function(keyCode,callbackFunction,allowRepeat) {
 	allowRepeat = typeof allowRepeat !== "undefined" ? allowRepeat : true;
 	
+	this._repeatableKey[keyCode] = allowRepeat;
+	console.log ("[DBG] key listener for " + keyCode + " (repeat : " + allowRepeat + ")");
 	var self = this;
 	$("*").keydown(function(codeKey){
-		if(self._keyPressed[keyCode] === 1) {
-			callbackFunction();
+		if (codeKey.keyCode === keyCode) {
+			// If the button is already pressed
+			if (self._keyPressed[keyCode] !== 1) {
+				self._keyPressed[keyCode] = 1;
+				// If the user allow the key  to be repeated
+				if(self._repeatableKey[keyCode]) {
+					callbackFunction();
+					self._repeatableKeyTimeOutID[keyCode] = setInterval(callbackFunction,17);
+					console.log("[DBG] Started process for " + keyCode + " with id : " + self._repeatableKeyTimeOutID[keyCode]);
+				}else{ // If he doesn't want it to be repeatable
+					callbackFunction();
+				}
+			}else{
+				//console.log("the key is already pressed so I do nothing");
+			}
+
 		}
 	});
+	
+	$("*").keyup(function(codeKey){
+		if (codeKey.keyCode === keyCode) {
+			self._keyPressed[keyCode] = 0;
+			// If the user allow the key  to be repeated
+			if(self._repeatableKey[keyCode]) {
+				clearTimeout(self._repeatableKeyTimeOutID[keyCode]);
+				console.log("[DBG] Ended process for " + keyCode + " with id : " + self._repeatableKeyTimeOutID[keyCode]);
+			}
+		}
+	});
+
 };
